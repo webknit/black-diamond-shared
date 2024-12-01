f3a1b3c6169f56e1e04c039de80a123d8c62b275
Shane Prendergast <contact@webknit.co.uk>
2024-12-01 18:57:15 +0000

    feat: enhance League interface with detailed stats structure

--- src/data/league/types/index.ts
+++ src/data/league/types/index.ts
@@ -7,6 +7,46 @@ export interface League {
     code?: string;
     flag?: string;
   };
-  stats?: any;
+  stats?: {
+    played: number;
+    goals: number;
+    wins: {
+      value: number;
+      percent: string;
+    };
+    draws: {
+      value: number;
+      percent: string;
+    };
+    nilnil: {
+      value: number;
+      percent: string;
+    };
+    BTTS: {
+      value: number;
+      percent: string;
+    };
+    over0Goal: {
+      value: number;
+      percent: string;
+    };
+    over1Goal: {
+      value: number;
+      percent: string;
+    };
+    over2Goal: {
+      value: number;
+      percent: string;
+    };
+    under2Goal: {
+      value: number;
+      percent: string;
+    };
+    under3Goal: {
+      value: number;
+      percent: string;
+    };
+    goalsPerGame: string;
+  };
   currentSeason?: number;
 }
