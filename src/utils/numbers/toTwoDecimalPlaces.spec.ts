c2d54665b89eaccd213ea0c1ef52418f4f6b4dc1
Shane Prendergast <contact@webknit.co.uk>
2024-12-28 20:21:30 +0000

    test: update toTwoDecimalPlaces tests for precision and output format

--- src/utils/numbers/toTwoDecimalPlaces.spec.ts
+++ src/utils/numbers/toTwoDecimalPlaces.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest";
 import { toTwoDecimalPlaces } from "./toTwoDecimalPlaces";
 
 describe("toTwoDecimalPlaces", () => {
+  // prettier-ignore
   const testCases = [
     { input: 1.23456, expected: 1.23 },
     { input: 2.00001, expected: 2 },
@@ -12,13 +13,14 @@ describe("toTwoDecimalPlaces", () => {
     { input: 2.37684, expected: 2.38 },
     { input: 2.37484, expected: 2.37 },
     { input: -1.0111111, expected: -1.01 },
-    { input: -1.004, expected: -1.0 },
-    { input: 0.0, expected: 0 },
+    { input: -1.004, expected: -1.000 },
+    { input: 0.0, expected: 0.0 },
     { input: 0, expected: 0 },
   ];
 
   testCases.forEach(({ input, expected }) => {
     it(`should round ${input} to ${expected}`, () => {
+      console.log(input, expected, toTwoDecimalPlaces(input));
       expect(toTwoDecimalPlaces(input) === expected).toBe(true);
     });
   });
