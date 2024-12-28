fe03a1469133e5d3fe9c62da52083debf88c0d72
Shane Prendergast <contact@webknit.co.uk>
2024-12-28 18:56:19 +0000

    feat: export calculateWinAfterCommission utility function

--- src/utils/numbers/index.ts
+++ src/utils/numbers/index.ts
@@ -1,3 +1,4 @@
 export { toTwoDecimalPlaces } from "./toTwoDecimalPlaces";
 export { toPercent } from "./toPercent";
 export { getMeanModeMedian } from "./getMeanModeMedian";
+export { calculateWinAfterCommission } from "./calculateWinAfterCommission";
