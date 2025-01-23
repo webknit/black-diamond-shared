0dd844780d2a53dbd5d85baf5ac329b35944ebed
Shane Prendergast <contact@webknit.co.uk>
2025-01-23 17:09:00 +0000

    feat: change form field types in TeamStat interface from arrays to strings

--- src/data/teamStat/types/index.ts
+++ src/data/teamStat/types/index.ts
@@ -67,9 +67,9 @@ export interface TeamStat extends MongoDocument {
       total: number;
     };
   };
-  form: ("W" | "L" | "D")[];
-  formHome: ("W" | "L" | "D")[];
-  formAway: ("W" | "L" | "D")[];
+  form: string;
+  formHome: string;
+  formAway: string;
   goals: {
     for: {
       total: {
