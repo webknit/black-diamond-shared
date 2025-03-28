1e79ed114e3d0da7403f5d2a0b345bd328db9f87
Shane Prendergast <contact@webknit.co.uk>
2025-03-28 11:03:02 +0000

    feat: export betStat from index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./data/team";
 export * from "./data/fixture";
 export * from "./data/teamStat";
 export * from "./data/bet";
+export * from "./data/betStat";
 export * from "./data/account";
 
 export * from "./utils/";
