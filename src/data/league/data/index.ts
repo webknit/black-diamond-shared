06379d987f1cbc64180b73dd5744a4284287b8d7
Shane Prendergast <contact@webknit.co.uk>
2025-05-17 19:19:21 +0100

    feat: comment out league IDs in LEAGUE_ID_LIST for clarity

--- src/data/league/data/index.ts
+++ src/data/league/data/index.ts
@@ -1,61 +1,61 @@
 export const LEAGUE_ID_LIST = [
-  39, // premier league
-  78, //Bundesliga 1
-  140, // La Liga
-  135, // Serie A
-  61, // Ligue 1,
-  188, // A league australia
-  40, // Championship
-  41, // League One
-  42, // League Two
-  43, //National League
-  141, // Segunda Division
-  79, //Bundesliga 2
-  136, // Serie B
-  62, // Ligue 2
-  179, // Scotland Premiership
-  180, // Scotland Championship,
-  113, // Sweden Allsvenskan
-  103, // Norway Eliteserien
-  94, // Primeira Liga portugal
-  71, // Serie A Brazil
-  72, // Serie B Brazil
-  253, // Major League Soccer
-  197, // Greece Super League
-  88, // Eredivisie Holland
-  144, // Belgium 1st div A
-  345, // Czech Liga
-  207, // Switz super league
-  98, // J1 League Japan
-  292, // K League 1
-  //333, // Ukraine premier league
-  128, // Argentine Primera División
-  169, // China super league
-  233, // Egypt premier leaguye
-  262, // Liga MX Mexico
-  244, // Veikkausliiga finland
-  164, // Iceland
-  281, // Peru Primera División
-  203, // Turkey Süper Lig
-  365, // Virsliga Latvia
-  357, // Ireland Premier division
-  106, // Polish Ekstraklasa
-  283, // Romania Liga 1
-  218, // Austria Bundesliga
-  119, // Denmark Superliga
-  344, // Bolivia Primera División
-  323, // Indian Super League
-  274, // Indonesia Liga 1
-  274, // Algeria Ligue 1
-  368, // singapore premier league
+  // 39, // premier league
+  // 78, //Bundesliga 1
+  // 140, // La Liga
+  // 135, // Serie A
+  // 61, // Ligue 1,
+  // 188, // A league australia
+  // 40, // Championship
+  // 41, // League One
+  // 42, // League Two
+  // 43, //National League
+  // 141, // Segunda Division
+  // 79, //Bundesliga 2
+  // 136, // Serie B
+  // 62, // Ligue 2
+  // 179, // Scotland Premiership
+  // 180, // Scotland Championship,
+  // 113, // Sweden Allsvenskan
+  // 103, // Norway Eliteserien
+  // 94, // Primeira Liga portugal
+  // 71, // Serie A Brazil
+  // 72, // Serie B Brazil
+  // 253, // Major League Soccer
+  // 197, // Greece Super League
+  // 88, // Eredivisie Holland
+  // 144, // Belgium 1st div A
+  // 345, // Czech Liga
+  // 207, // Switz super league
+  // 98, // J1 League Japan
+  // 292, // K League 1
+  // //333, // Ukraine premier league
+  // 128, // Argentine Primera División
+  // 169, // China super league
+  // 233, // Egypt premier leaguye
+  // 262, // Liga MX Mexico
+  // 244, // Veikkausliiga finland
+  // 164, // Iceland
+  // 281, // Peru Primera División
+  // 203, // Turkey Süper Lig
+  // 365, // Virsliga Latvia
+  // 357, // Ireland Premier division
+  // 106, // Polish Ekstraklasa
+  // 283, // Romania Liga 1
+  // 218, // Austria Bundesliga
+  // 119, // Denmark Superliga
+  // 344, // Bolivia Primera División
+  // 323, // Indian Super League
+  // 274, // Indonesia Liga 1
+  // 274, // Algeria Ligue 1
+  // 368, // singapore premier league
   307, // saudi pro league
-  2, // UEFA Champions League
-  3, // UEFA Europa League
-  1, //World Cup
-  4, // EURO Championship
-  8, //World Cup - Women
-  5, // UEFA Nations League
-  848, // UEFA Europa Conference League
-  45, // FA Cup
-  48, // League Cup (Caribo Cup)
+  // 2, // UEFA Champions League
+  // 3, // UEFA Europa League
+  // 1, //World Cup
+  // 4, // EURO Championship
+  // 8, //World Cup - Women
+  // 5, // UEFA Nations League
+  // 848, // UEFA Europa Conference League
+  // 45, // FA Cup
+  // 48, // League Cup (Caribo Cup)
 ];
